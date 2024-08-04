62ccdc399946e7c5a797bdd0cd3e4c90701015ca
ahmadasnawi <ahmadaries899@gmail.com>
2024-08-04 09:23:24 +0700

    first commit

---
+++ tugas.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <section>
+        <div class="left">
+            <div class="top">
+                <div class="atas">
+                    <i class="fa-solid fa-feather"></i>
+                    <h1>belzioo</h1>
+                    <h2>EN</h2>
+                </div>
+                <div class="tengah">
+                    <h1>Explore</h1>
+                </div>
+                <div class="bawah">
+                    <button>
+                        <i class="fa-solid fa-table"></i>
+                        <p>Categories</p>
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="mid">
+                <div class="icon a">
+                    <i class="fa-solid fa-medal"></i>
+                    <p>Best Offers</p>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-shop"></i>
+                    <p>Sell with Us</p>
+                </div>
+                <div class="icon">
+                    <i class="fa-sharp-duotone fa-solid fa-truck"></i>
+                    <p>Track Order</p>
+                </div>
+                <div class="icon b">
+                    <i class="fa-sharp-duotone fa-solid fa-file-invoice"></i>
+                    <p>Blog</p>
+                </div>
+            </div>
+            <div class="bottom mid">
+                <div class="garis"></div>
+                <div class="icon">
+                    <i class="fa-sharp-duotone fa-solid fa-user-pen"></i>
+                    <p>Account</p>
+                </div>
+                <div class="icon">
+                    <i class="fa-sharp-duotone fa-solid fa-gear"></i>
+                    <p>Settings</p>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <nav>
+                <div class="left-line">
+                    <select name="" id="">
+                        <option value="1">All Categoaries</option>
+                        <option value="2">Shop</option>
+                        <option value="3">Store</option>
+                    </select>
+                    <input type="text" placeholder="Search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <div class="mid-line">
+                    <i class="fa-solid fa-bell"></i>
+                    <i class="fa-solid fa-heart"></i>
+                    <i class="fa-solid fa-bag-shopping"></i>
+                </div>
+                <div class="right-line">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+            </nav>
+            <header>
+                <div class="samping-kiri">
+                    <img src="./img/men.png" alt="">
+                </div>
+                <div class="tengah-tengah">
+                    <div class="box">
+                        <div class="atas">
+                            <span></span>
+                            <h3>Special Offers</h3>
+                        </div>
+                        <h1>Men Shoes</h1>
+                        <p>Best Shoes Discount <b>35% OFF</b> On all Men Shoes!</p>
+                        <div class="brend">
+                            <img src="./img/adidas.png" alt="">
+                            <img src="./img/nike.png" alt="">
+                            <img src="./img/fila.png" alt="">
+                            <div class="kotak-kecil">
+                                <i class="fa-solid fa-chevron-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="samping-kanan">
+                    <img src="./img/promo.png" alt="">
+                </div>
+            </header>
+            <div class="judul">
+                <h1>Top <b>Products</b></h1>
+                <p>View All</p>
+                <i class="fa-solid fa-chevron-left"></i>
+                <i class="fa-solid fa-chevron-right"></i>
+            </div>
+            <div class="produk">
+                <div class="kotak-luar">
+                    <div class="shoes-pink">
+                        <p>Free Shipping</p>
+                        <img src="./img/shoes1.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Paarmodelle All Might <br>Wanderschuhe</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-1"></div>
+                        <div class="warna-2"></div>
+                        <div class="harga">$39.68</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-hitam">
+                        <img src="./img/shoes4.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Converse All Star<br>1970s Hi</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-3"></div>
+                        <div class="warna-4"></div>
+                        <div class="harga">$89.00</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-coklat">
+                        <p>%</p>
+                        <img src="./img/shoes2.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Hoka One Clifton <br>L Suede</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-5"></div>
+                        <div class="warna-6"></div>
+                        <div class="harga">$45.00</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-pelangi">
+                        <img src="./img/shoes3.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Vans UltraRange <br>Exo MTE-1</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-7"></div>
+                        <div class="warna-8"></div>
+                        <div class="harga">$39.68</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-pink">
+                        <p>Free Shipping</p>
+                        <img src="./img/shoes1.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Paarmodelle All Might <br>Wanderschuhe</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-1"></div>
+                        <div class="warna-2"></div>
+                        <div class="harga">$39.68</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-hitam">
+                        <img src="./img/shoes4.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Converse All Star<br>1970s Hi</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-3"></div>
+                        <div class="warna-4"></div>
+                        <div class="harga">$89.00</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-coklat">
+                        <p>%</p>
+                        <img src="./img/shoes2.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Hoka One Clifton <br>L Suede</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-5"></div>
+                        <div class="warna-6"></div>
+                        <div class="harga">$45.00</div>
+                    </div>
+                </div>
+                <div class="kotak-luar">
+                    <div class="shoes-pelangi">
+                        <img src="./img/shoes3.png" alt="">
+                    </div>
+                    <div class="nama-produk">
+                        <h3>Vans UltraRange <br>Exo MTE-1</h3>
+                    </div>
+                    <div class="keterangan">
+                        <div class="warna-7"></div>
+                        <div class="warna-8"></div>
+                        <div class="harga">$39.68</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
